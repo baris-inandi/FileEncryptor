7bc8a7f15b0c7499c7c6a382a820f45fe20ebd26
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2021-03-26 12:30:10 +0300

    update update.py

--- update/update.py
+++ update/update.py
@@ -2,13 +2,27 @@ import requests
 import os
 from time import sleep as delay
 from threading import Thread
+import platform
+
+repo_name = "fileencryptor"
+
+def something_went_wrong():
+    print("something went wrong")
+
+def generateurl(os):
+    if os == "windows": os_id, os_filetype = "win", ".exe"
+    elif os == "linux": os_id, os_filetype = "linux", ".tar.gz"
+    elif os == "darwin": os_id, os_filetype = "macos", ".dmg"
+    else: something_went_wrong()
+    return f'https://github.com/baris-inandi/fileencryptor/releases/latest/download/{repo_name}-{os_id}{os_filetype}'
 
 def download():
-    url = "https://github.com/baris-inandi/fileencryptor/releases/latest/download/InstallFileEncryptor.exe"
+    url = generateurl(str(platform.system().lower()))
     r = requests.get(url, allow_redirects=True)
     open("./download.exe", 'wb').write(r.content)
     os.system("start ./download.exe")
     raise Exception
+
 def loading():
     while True:
         message = "Installing update"
