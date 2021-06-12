f7280c3e57b435857f8d3bcf854bbbd5ba4e6e26
Barış İnandıoğlu <binandioglu2006@gmail.com>
2021-06-13 02:11:56 +0300

    Add pop-ups to gui (warnings, password prompts vs.)

--- gui.py
+++ gui.py
@@ -5,6 +5,18 @@ class App():
     encrypted_filetype = ".aes"
     tempdir = tempfile.gettempdir()
 
+@eel.expose
+def hello():
+    print("Hello")
+
+"""
+root = Tk()
+root.withdraw()
+root.wm_attributes('-topmost', 1)
+folder = filedialog.askdirectory()
+return folder
+"""
+
 #initialize gui
 eel.init("gui")
 eel.start('index.html', size=(500, 520))
--- gui/main.js
+++ gui/main.js
@@ -1,5 +1,3 @@
-encryptedFiletype = ".aes"
-
 $(document).ready(function (){
     window.resizeTo(500, 520);
     window.focus();
@@ -9,15 +7,6 @@ $(document).ready(function (){
     document.body.style.zoom = "100%";
 });
 
-$('#dec-btn-real').prop('accept', encryptedFiletype);
-
-document.getElementById("enc-btn").addEventListener("click", function() {
-    document.getElementById("enc-btn-real").click();
-});
-document.getElementById("dec-btn").addEventListener("click", function() {
-    document.getElementById("dec-btn-real").click();
-});
-
-document.getElementById('enc-btn-real').onchange = function () {
-    alert('Selected file: ' + this.value);
-  };
+function py(){
+    eel.hello()
+}
